0fa1851747217a01fccc68bc64323b76c47d84ee
Andy B <me@andybritcliffe.com>
2014-08-20 10:53:21 +0100

    extended initial euclidean func

--- recommendations.rb
+++ recommendations.rb
@@ -1,7 +1,7 @@
 include Math
 
 class Coll_intell
-   #uses values as a comparison on the chart. Take the difference in each axis, square them, add them together, then take the square root of the sum. 
+   #Euclidean Distance uses values as a comparison on a x/y chart to compare two items (ronaldo vs messi for eg). Take the difference in each axis, square them, add them together, then take the square root of the sum. Higher value means closer relationship
   def calc_euclidean x,y,x1,y1
     1/(1+sqrt( (x-x1)**2 + (y-y1)**2  ))
   end
