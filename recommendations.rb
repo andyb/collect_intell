6b000cdb67d9b03f76c87ea68c7af08b8f3d5daa
Andy B <me@andybritcliffe.com>
2014-08-20 10:51:41 +0100

    created initial euclidean func

--- recommendations.rb
+++ recommendations.rb
@@ -1,8 +1,8 @@
 include Math
 
 class Coll_intell
-   #uses values as a comparison on the chart. Take the difference in each axis, square them, add them together, then take the square root of the sum
+   #uses values as a comparison on the chart. Take the difference in each axis, square them, add them together, then take the square root of the sum. 
   def calc_euclidean x,y,x1,y1
-    sqrt( (x-x1)**2 + (y-y1)**2  )
+    1/(1+sqrt( (x-x1)**2 + (y-y1)**2  ))
   end
 end
